dd2120aacb70e5bf872c317811249652475ba381
sesinos tesfamariam <sesinostesfamariam@sesinoss-mbp.attlocal.net>
2019-05-08 13:49:13 -0700

    adding-changes

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -4,12 +4,61 @@ import React from "react";
 import Todo from "./Todo";
 import TodoForm from "./TodoForm";
 
+const students = [
+    {
+        task: 'Organize Garage',
+        id: 1528817077286,
+        completed: false
+    },
+    {
+        task: 'Bake Cookies',
+        id: 1528817084358,
+        completed: false
+    }
+];
+
 class TodoList extends React.Component {
     constructor() {
         super();
         this.state = {
-            chores: "",
-        }
+            todoList: listitems,
+            name: "",
+        };
+    }
+
+    addListItem = e => {
+        e.preventDefault();
+        this.setState({
+            todoList: [
+            ...this.state.todoList,
+            {
+                name: this.state.name,
+            }
+          ]
+        });
+    };
+
+    changeHandler = e => {
+        console.log(e.target.value);
+        this.setState({ [e.target.name]: e.target.value });
+      };
+
+      render() {
+        return (
+          <div>
+            <h1>Todo List: MVP</h1>
+            <div className="class-list">
+              {this.state.todoList.map(todo => (
+                <Todo todo={todo} />
+              ))}
+            </div>
+            <TodoForm
+              addListItem={this.addListItem}
+              changeHandler={this.changeHandler}
+              name={this.state.name}
+            />
+          </div>
+        );
     }
 }
 
